574d8e136d7969e0c34da6f9748d733133d9ae8f
ranyme mejdoub <ranymee@icloud.com>
2021-11-29 11:52:12 +0100

    third commit

---
+++ checkpoint_react/src/App.js
@@ -0,0 +1,49 @@
+
+import './App.css';
+import {Navbar, container ,Form, button} from  'react-bootstrap';
+function App() {
+  return (
+    <div className="App">
+ <h1> Checkpoint React</h1>
+ <Navbar>
+  <Container>
+    <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+    <Navbar.Toggle />
+    <Navbar.Collapse className="justify-content-end">
+      <Navbar.Text>
+        Signed in as: <a href="#login">Mark Otto</a>
+      </Navbar.Text>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+<Container fluid="md">
+  <Row>
+    <Col>1 of 1</Col>
+  </Row>
+</Container>
+
+ <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+    </div>
+  );
+}
+
+export default App;
